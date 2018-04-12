bc9e57d9faaf6b1492a9f19574f28db7ce80b0dc
LuoMin <luomin5417@gmail.com>
2018-04-12 18:25:05 +0800

    1.添加koala

---
+++ koala/mylexer.py
@@ -0,0 +1,22 @@
+import ply.lex as lex
+
+tokens = (
+    'number',
+    'plus',
+    'minus',
+    'multiply',
+    'divide',
+    'lparen',
+    'rparen',
+    'assign',
+    'equal',
+    'greater',
+    'less',
+    'colon',
+    'pound',
+    'dot',
+    'variate'
+)
+
+t_plus = r'\+'
+t_minus =
